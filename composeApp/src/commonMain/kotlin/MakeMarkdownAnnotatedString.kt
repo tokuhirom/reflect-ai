6999a5deabc7455ac617ce63eb45fda510dd733b
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-11-13 17:16:13 +0900

    support bold

--- composeApp/src/commonMain/kotlin/MakeMarkdownAnnotatedString.kt
+++ composeApp/src/commonMain/kotlin/MakeMarkdownAnnotatedString.kt
@@ -2,11 +2,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 
 fun makeMarkdownAnnotatedString(inputText: String): AnnotatedString {
-    val pattern = """(https?://[^\s\]]+)|(\*\*(.*?)\*\*)""".toRegex()
+    val pattern = """(https?://[^\s\]]+)|(\*\*.*?\*\*)""".toRegex()
     val matches = pattern.findAll(inputText).toList()
 
     val annotatedText = buildAnnotatedString {
@@ -14,20 +15,38 @@ fun makeMarkdownAnnotatedString(inputText: String): AnnotatedString {
             val matchStart = matchResult.range.first
             val matchEnd = matchResult.range.last
 
+            val urlGroup = matchResult.groups[1]
+            val boldGroup = matchResult.groups[2]
+            println("CURRENT: $urlGroup, $boldGroup")
+
             append(inputText.substring(lastEnd, matchStart))
 
-            pushStringAnnotation(
-                tag = "URL",
-                annotation = inputText.substring(matchStart, matchEnd + 1)
-            )
-
-            withStyle(
-                style = SpanStyle(
-                    color = Color.Blue,
-                    textDecoration = TextDecoration.Underline
+            if (urlGroup != null) {
+                pushStringAnnotation(
+                    tag = "URL",
+                    annotation = inputText.substring(matchStart, matchEnd + 1)
                 )
-            ) {
-                append(inputText.substring(matchStart, matchEnd + 1))
+
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Blue,
+                        textDecoration = TextDecoration.Underline
+                    )
+                ) {
+                    append(inputText.substring(matchStart, matchEnd + 1))
+                }
+            } else {
+                pushStringAnnotation(
+                    tag = "BOLD",
+                    annotation = inputText.substring(matchStart, matchEnd + 1)
+                )
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.Bold,
+                    )
+                ) {
+                    append(inputText.substring(matchStart + 2, matchEnd + 1 - 2))
+                }
             }
 
             pop()
